2bab50023978bf2652474a47bad64465b0dad876
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-02-04 22:58:36 -0800

    Preparing to try GH actions.

--- step0_sleep/sleep/run.sh
+++ step0_sleep/sleep/run.sh
@@ -22,8 +22,8 @@ trap cb_sigint SIGINT
 trap cb_sigterm SIGTERM
 trap cb_sigquit SIGQUIT
 
-N=60
-for i in $(seq $N); do
-  echo $i / $N
-  sleep 1
+ACTUAL_N=${N:-60}
+for i in $(seq $ACTUAL_N); do
+  echo $i / $ACTUAL_N
+  sleep 0.1
 done
